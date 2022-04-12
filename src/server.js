ab9972afa88a542b271f149b4f795732d6b67baa
frinica <frida.nicander@chasacademy.se>
2022-04-13 01:03:06 +0200

    new update

--- src/server.js
+++ src/server.js
@@ -14,5 +14,3 @@ app.use(express.static("./dist/<name-on-package.json>"));
 app.get("/*", function (req, res) {
   res.sendFile("index.html", { root: "dist/<name-on-package.json>/" });
 });
-
-app.listen(process.env.PORT || 8080);
